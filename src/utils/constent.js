fc6ddc26e47bdb11867f0f1ab861b05250bf7100
Ankit Kumar Yogi <85799985+nikkiankityogi18@users.noreply.github.com>
2023-07-13 19:46:10 +0530

    http to https search suggestion

--- src/utils/constent.js
+++ src/utils/constent.js
@@ -3,7 +3,7 @@ export const YOUTUBE_VIDEOS_API =
   process.env.REACT_APP_GOOGLE_API_KEY;
 
 export const YOUTUBE_SEARCH_SUGGESTION_API =
-  "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
+  "https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
 
 export const YOUTUBE_POPULAR_VIDEO_SUGGESTION_API =
   "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=10&regionCode=IN&key=" +
